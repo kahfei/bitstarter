89521c25db7c66dc0365e1e001fbb4bedbc86264
kahfei <bak.kahfei@gmail.com>
2013-07-24 14:30:58 +0000

    modify web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+var fs = require('fs');
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+fs.readFile('index.html', function(err, data) {
+  if(err) throw err;
+  var buf = new Buffer(data);
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
