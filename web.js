5eaf084bce03dc03f4a90073713716cda17ab814
kahfei <bak.kahfei@gmail.com>
2013-07-24 16:39:00 +0000

    working on web.js

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var fs = require('fs');
-
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buf;
 fs.readFile('index.html', function(err, data) {
   if(err) throw err;
-  var buf = new Buffer(data);
+  buf = new Buffer(data);
 });
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
