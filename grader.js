5ebf046a98b0d04a6146f6b91c4e18bfe2ed5ac0
kahfei <bak.kahfei@gmail.com>
2013-07-29 22:19:43 +0000

    added all css and javascript dependencies

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs =  require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -64,12 +65,27 @@ var clone = function(fn) {
 if(require.main == module) {
   program
       .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-      .option('-f, --file <html_file>', 'PAth to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+      .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+      .option('-u, --url <url>', 'Path to website')
       .parse(process.argv);
+  if (program.url) {
+    restler.get(program.url).on('complete', function(data) {
+      var out = {};
+      $ = cheerio.load(data);
+      var checks = loadChecks(program.checks).sort();
+      for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+      }
+      var outJson = JSON.stringify(out, null, 4);
+      console.log(outJson);
+    });
+}else{
   var checkJson = checkHtmlFile(program.file, program.checks);
   var outJson = JSON.stringify(checkJson, null, 4);
   console.log(outJson);
-} else {
+
+}} else {
     exports.checkHtmlFile = checkHtmlFile;
 }
 
